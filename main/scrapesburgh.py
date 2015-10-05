5f0342a52ff2bc88e015c92feff9ac4c17f6cd94
Jacob Sanders <j.q.sanders@gmail.com>
2015-10-05 18:17:46 -0400

    handled some edge cases and name normalization. much closer to db storage

--- main/scrapesburgh.py
+++ main/scrapesburgh.py
@@ -7,6 +7,8 @@ import re
 
 class ScrapesBurgh():
 
+    # checks the url to make sure it's valid
+    # and, if so, reads in the html
     @staticmethod
     def open_url(url):
         click = urllib.urlopen(url)
@@ -17,6 +19,7 @@ class ScrapesBurgh():
             raise Exception("Nice try. Bad link.")
             # pass
 
+    # initial parse of the html
     @staticmethod
     def parse_html(html):
         parser = etree.HTMLParser()
@@ -24,6 +27,7 @@ class ScrapesBurgh():
 
         return master_tree
 
+    # gets the parsed tree to begin the scrape
     @staticmethod
     def treeify(url):
         html = ScrapesBurgh.open_url(url)
@@ -31,6 +35,7 @@ class ScrapesBurgh():
 
         return tree
 
+    # gets all the ids we'll need to create the scraping urls dynamically
     @staticmethod
     def all_urls(url):
         tree = ScrapesBurgh.treeify(url)
@@ -48,6 +53,7 @@ class ScrapesBurgh():
 
         return tree, board_ids
 
+    # creates the urls to scrape and opens each, beginning the scrape
     @staticmethod
     def scrape_data(board_ids):
         # print board_ids
@@ -55,14 +61,10 @@ class ScrapesBurgh():
             url = "http://www.alleghenycounty.us/boards/index.asp?Board=%d&button1=View" % board_id
             tree = ScrapesBurgh.treeify(url)
             print url
-            ScrapesBurgh.get_board_info(url, tree)
+            board_name, history, creation, contact, address, meeting_place, meeting_time, phone, email, link, members = ScrapesBurgh.get_board_info(url, tree)
+            ScrapesBurgh.clean_board_info(url, board_name, history, creation, contact, address, meeting_place, meeting_time, phone, email, link, members)
 
-    @staticmethod
-    def start_scrape():
-        tree, board_ids = ScrapesBurgh.all_urls('http://www.alleghenycounty.us/boards/index.asp')
-
-        ScrapesBurgh.scrape_data(board_ids)
-
+    # points the scraper in the direction of the data it needs
     @staticmethod
     def get_board_info(url, tree):
 
@@ -79,11 +81,10 @@ class ScrapesBurgh():
         link_xpath = '//*[@id="form1"]/blockquote/table[1]/tr[1]/td/font/b/a/@href'
         members_xpath = '//*[@id="form1"]/blockquote/table[2]/tr/td/font/text()'
 
-        if tree.xpath(board_name_xpath) is not None:
-            board_name_xpath = '//*[@id="form1"]/blockquote/table[1]/tr[1]/td/font/b/text()'
+        if len(tree.xpath(board_name_xpath)) != 1:
+            board_name_xpath = '//*[@id="form1"]/blockquote/table[1]/tr[1]/td/font/b/a/text()'
 
         board_name = tree.xpath(board_name_xpath)
-
         history = tree.xpath(history_xpath)
         creation = tree.xpath(creation_xpath)
         contact = tree.xpath(contact_xpath)
@@ -95,6 +96,13 @@ class ScrapesBurgh():
         link = tree.xpath(link_xpath)
         members = tree.xpath(members_xpath)
 
+        return board_name, history, creation, contact, address, meeting_place, meeting_time, phone, email, link, members
+
+    # cleans up the filthy, filthy data to prepare for storage
+    # PTOOEY. so dirty. your mother would be ashamed.
+    @staticmethod
+    def clean_board_info(url, board_name, history, creation, contact, address, meeting_place, meeting_time, phone, email, link, members):
+
         board_name = board_name[0].strip()
 
         try:
@@ -102,50 +110,27 @@ class ScrapesBurgh():
         except:
             pass
 
-        if contact:
-            contact = contact[0].replace(u'\xa0', u' ')
-        else:
-            print "No contact name"
+        contact = contact[0].replace(u'\xa0', u'')
+        meeting_place = meeting_place[0].replace(u'\xa0', u'')
+        meeting_time = meeting_time[0].replace(u'\xa0', u'')
+        phone = phone[0].replace(u'\xa0', u'')
 
-        if meeting_place:
-            meeting_place = meeting_place[0].replace(u'\xa0', u' ')
-        else:
-            print "No meeting place"
+        address_list = []
+        for addr in address:
+            addr = addr.strip()
+            address_list.append(addr)
 
-        if meeting_time:
-            meeting_time = meeting_time[0].replace(u'\xa0', u' ')
-        else:
-            print "No meeting time"
+        members_dict = {}
+        no_members_dict = {}
 
-        if phone:
-            phone = phone[0].replace(u'\xa0', u' ')
-        else:
-            print "No phone number"
+        for member in members:
+            member = member.strip()
 
-        if address:
-            address_list = []
-            for addr in address:
-                addr = addr.strip()
-                address_list.append(addr)
-        else:
-            print "No address"
+            name_pattern = '([A-Z][^\s,]+)'
+            name_match = re.findall(name_pattern, '%s' % member)
 
-        if members:
-            members_dict = {}
-
-            for member in members:
-                member = member.strip()
-
-                name_pattern = '([A-Z]\w+)'
-                name_match = re.findall(name_pattern, '%s' % member)
-                if len(name_match) == 1:
-                    first_initial_pattern = '([A-Z]\.)'
-                    first_initial_match = re.findall(first_initial_pattern, '%s' % member)
-                    name = first_initial_match[0] + ' ' + name_match[0]
-                else:
-                    name = name_match[1] + ' ' + name_match[0]
-                    # print name
-
+            if "Members" not in name_match:
+                name = name_match[1].replace(u'\xa0', u'') + ' ' + name_match[0]
                 date_pattern = '(\d+/\d+/\d+)'
                 date_match = re.search(date_pattern, '%s' % member)
                 if date_match:
@@ -153,8 +138,19 @@ class ScrapesBurgh():
                     members_dict[name] = (board_name, date_final)
                 else:
                     members_dict[name] = (board_name, "No end of term given")
-        else:
 
-            print "This board had no members"
+            # elif len(name_match) == 1 and "Members" not in name_match:
+            #     first_initial_pattern = '([A-Z]\.)'
+            #     first_initial_match = re.findall(first_initial_pattern, '%s' % member)
+            #     name = first_initial_match[0] + ' ' + name_match[0]
+            else:
+                no_members_dict[board_name] = ("%s has no members" % board_name, url)
 
-        print board_name, '\n', history, '\n', creation, '\n', members_dict, '\n', contact, '\n', link, '\n', address_list, '\n', meeting_place, '\n', meeting_time, '\n', phone, '\n', email
+        print board_name, '\n', history, '\n', creation, '\n', members_dict, '\n', contact, '\n', link, '\n', address_list, '\n', meeting_place, '\n', meeting_time, '\n', phone, '\n', email, '\n', no_members_dict
+
+    # starts the whole ball o' wax
+    @staticmethod
+    def start_scrape():
+        tree, board_ids = ScrapesBurgh.all_urls('http://www.alleghenycounty.us/boards/index.asp')
+
+        ScrapesBurgh.scrape_data(board_ids)
